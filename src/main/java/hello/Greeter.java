fc09786ca7ebc61ca58e94a9e05cc2304c81597a
TheDevrajDas <58029145+TheDevrajDas@users.noreply.github.com>
2022-04-28 13:50:54 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Devraj Das !! Hi or Bye hello is the string needed here.";
+    return "Hello new world- This is Devraj Das !! Hi or Bye hello is the string needed here.";
   }
 }
